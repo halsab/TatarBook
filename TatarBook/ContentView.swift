eaacb3b60210eb1188b56eeac85cf59142fc2948
khalil_sabirov <61316574+halsab@users.noreply.github.com>
2022-03-23 18:38:21 +0300

    Initial Commit

---
+++ TatarBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TatarBook
+//
+//  Created by Ha Sab on 23.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TatarBook/TatarBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TatarBookApp.swift
+//  TatarBook
+//
+//  Created by Ha Sab on 23.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TatarBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
