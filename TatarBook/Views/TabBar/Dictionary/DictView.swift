39b3df8b3965a08392878c3d8885173247dc08fb
khalil_sabirov <61316574+halsab@users.noreply.github.com>
2022-06-01 17:52:52 +0300

    Improved and refactored words searching

---
+++ TatarBook/Views/TabBar/Dictionary/DictView.swift
@@ -0,0 +1,20 @@
+//
+//  DictView.swift
+//  TatarBook
+//
+//  Created by halsab on 01.06.2022.
+//
+
+import SwiftUI
+
+struct DictView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DictView_Previews: PreviewProvider {
+    static var previews: some View {
+        DictView()
+    }
+}
---
+++ TatarBook/Views/TabBar/Dictionary/DictViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  DictViewModel.swift
+//  TatarBook
+//
+//  Created by halsab on 01.06.2022.
+//
+
+import SwiftUI
+
+struct DictViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DictViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        DictViewModel()
+    }
+}
