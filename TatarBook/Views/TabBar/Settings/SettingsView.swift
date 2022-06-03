04556f2cb1708a1f0dfb82e6567a09464d88288c
khalil_sabirov <61316574+halsab@users.noreply.github.com>
2022-06-03 09:22:44 +0300

    Small fixes in settings

--- TatarBook/Views/TabBar/Settings/SettingsView.swift
+++ TatarBook/Views/TabBar/Settings/SettingsView.swift
@@ -28,19 +28,6 @@ struct SettingsView: View {
                     TintColorPicker(tintColor: $appManager.tintColor)
                 }
                 Section {
-                    VStack {
-                        VersionRowView(name: "Кушымта", version: appVersion)
-                        ForEach(appManager.config.files) { file in
-                            Divider()
-                            VersionRowView(name: appManager.config.getPrettyName(of: file), version: file.version)
-                        }
-                    }
-                    .padding(.vertical, 8)
-                } header: {
-                    Text("Версиялар")
-                        .font(.system(.footnote, design: .serif))
-                }
-                Section {
                     VStack(spacing: 8) {
                         Text("Бу кушымта «Иман» нәшрияте тарафыннан бастыралган «Иске татар имлясы буенча дәреслек» нигезендә ясалды.")
                         Text("Китапны төзүче: «Мөхәммәдия» мәдрәсәсе мөгаллиме, Апанай мәчетенең имамы, Әхмәт хәзрәт Сабыр.")
@@ -49,6 +36,26 @@ struct SettingsView: View {
                     .font(.system(.callout, design: .serif))
                     .foregroundColor(.secondary)
                     .multilineTextAlignment(.center)
+                }
+                Section {
+                    HStack {
+                        Spacer()
+                        VStack(alignment: .leading) {
+                            Text("Кушымта ")
+                            ForEach(appManager.config.files) { file in
+                                Text(appManager.config.getPrettyName(of: file) + " ")
+                            }
+                        }
+                        VStack(alignment: .trailing) {
+                            Text(appVersion)
+                            ForEach(appManager.config.files) { file in
+                                Text(file.version)
+                            }
+                        }
+                        Spacer()
+                    }
+                    .font(.system(.footnote, design: .monospaced))
+                    .foregroundColor(.secondary)
                 }
             }
             .navigationTitle(Text("Көйләнеш"))
